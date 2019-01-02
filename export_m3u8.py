898788df03f7cc5ea6fc7391e72365c6647b0156
alex-phillips <ahp118@gmail.com>
2019-01-02 10:51:09 -0500

    added option to specify playlist without a prompt

--- export_m3u8.py
+++ export_m3u8.py
@@ -11,7 +11,16 @@ PLEX_TOKEN = CONFIG.data['auth'].get('server_token', PLEX_TOKEN)
 plex = PlexServer(PLEX_URL, PLEX_TOKEN)
 
 playlists = [pl for pl in plex.playlists() if pl.isAudio]
-playlist = utils.choose('Playlist to export', playlists, lambda pl: '%s' % pl.title)
+playlist = None
+if sys.argv[1]:
+    for pl in playlists:
+        if pl.title == sys.argv[1]:
+            playlist = pl
+    if playlist is None:
+        print("Invalid playlist '{}'".format(sys.argv[1]))
+        sys.exit()
+else:
+    playlist = utils.choose('Playlist to export', playlists, lambda pl: '%s' % pl.title)
 
 for song in playlist.items():
     print(song.media[0].parts[0].file)
